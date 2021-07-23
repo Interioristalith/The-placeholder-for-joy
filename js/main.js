5511103bbafdbec5d3f88e1484a155c212d69662
Vik Paw <aligsentjames@gmail.com>
2021-07-23 20:56:15 +0300

    Starting  a new project.

---
+++ js/main.js
@@ -0,0 +1,19 @@
+// Microscroll of background while scrolling the webpage
+
+// Expanding the side panel
+
+function expandPanel () {
+    const leftPanel = document.getElementById('left-panel')
+    leftPanel.addEventListener('mouseenter', slideRight)
+
+    leftPanel.addEventListener('mouseleave', slideBack)
+
+    function slideRight () {
+        leftPanel.classList.toggle('left-panel__expanded')
+    }
+
+    function slideBack () {
+        leftPanel.classList.toggle('left-panel__expanded')
+    }
+}
+expandPanel();
