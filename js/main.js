1de1af1e0e0c0d8954943a52285798bc12c6ea77
vittypaw <interioristalit@icloud.com>
2021-07-24 18:50:18 +0300

    Trying out anime.js library.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 // Microscroll of background while scrolling the webpage
 
+// const { easings } = require("animejs")
+
+
 // Expanding the side panel
 
 function expandPanel () {
@@ -17,3 +20,59 @@ function expandPanel () {
     }
 }
 expandPanel();
+
+
+// Temporary scrolling down
+
+window.scroll(0, 200)
+window.onload = scrollDown;
+
+function scrollDown () {
+    window.scroll(0, document.body.scrollHeight)
+}
+
+
+// Anime.js
+
+const container = document.querySelector('.container')
+for (let i=0; i <=100; i++) {
+    const blocks = document.createElement('div')
+    blocks.classList.add('block')
+    container.appendChild(blocks)
+}
+
+
+
+function getWidth() {
+    const first = document.getElementsByClassName('anime-js')[0]
+    let width = window.getComputedStyle(first).width
+    return width;
+}
+
+function getHeight() {
+    const first = document.getElementsByClassName('anime-js')[0]
+    let height = window.getComputedStyle(first).height
+    return height;
+}
+
+let param = [parseInt(getWidth())/2 - 25, parseFloat(getHeight())/2 - 50]
+
+function animateBlocks () {
+    anime ({
+        targets: '.block',
+        translateX: function () {
+            return anime.random((-1)*param[0], param[0])
+        },
+        translateY: function () {
+            return anime.random(-1*param[1], param[1])
+        },
+        scale: function () {
+            return anime.random(1,1.5)
+        },
+        easing: 'linear',
+        duration: 3000,
+        delay: anime.stagger(10),
+        complete: animateBlocks
+    })
+}
+animateBlocks()
